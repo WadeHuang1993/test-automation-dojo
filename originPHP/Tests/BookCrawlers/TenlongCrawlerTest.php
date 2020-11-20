e73dcfb17f4d77b41ea1c2da6d3cd7baf7552865
zang <myginnygo@gmail.com>
2020-11-20 17:22:43 +0800

    實作 TenlongCrawlerTest

--- originPHP/Tests/BookCrawlers/TenlongCrawlerTest.php
+++ originPHP/Tests/BookCrawlers/TenlongCrawlerTest.php
@@ -1,6 +1,8 @@
 <?php
 
+namespace Tests\BookCrawlers;
 
+use PHPUnit\Framework\TestCase;
 use Src\BookCrawlers\TenlongCrawler;
 
 /**
@@ -14,8 +16,18 @@ use Src\BookCrawlers\TenlongCrawler;
  *
  * 請依照使用案例開出來的需求規格（given、when、then）來完成測試案例。
  */
-class TenlongCrawlerTest
+class TenlongCrawlerTest extends TestCase
 {
+
+    /**
+     * 測試取得網頁 html
+     */
+    public function testGetHtml()
+    {
+        $target = new TenlongCrawler();
+        $html = $target->getHtml();
+        $this->assertNotEmpty($html);
+    }
 
     /**
      * 使用案例：
@@ -30,14 +42,14 @@ class TenlongCrawlerTest
      * Tips4: 盡可能將複雜的實作邏輯封裝進受測物件內
      * Tips5: 盡可能設計出可讀性高的介面
      */
-    public function testXXX()
+    public function testFindBookByName()
     {
         /** @given 要搜集的書籍名稱：領域驅動設計 */
         $bookName = '領域驅動設計';
 
         /** @when 系統透過爬蟲向天瓏圖書執行關鍵字搜尋 */
-        // 爬蟲的目標 URL: https://www.tenlong.com.tw/search?utf8=✓&keyword=領域驅動設計
         $crawler = new TenlongCrawler();
+        $book = $crawler->findBookByName($bookName);
 
         /** @then 爬蟲找到書籍並擷取出書籍內容 */
         // 完整書名：領域驅動設計：軟體核心複雜度的解決方法 (Domain-Driven Design: Tackling Complexity in the Heart of Software)
@@ -48,11 +60,40 @@ class TenlongCrawlerTest
         // 售價：530
         // 超連結：https://www.tenlong.com.tw/products/9789864343874?list_name=srh
         // 縮圖位置：https://cf-assets2.tenlong.com.tw/products/images/000/130/774/medium/9789864343874_bc.jpg?1554177163
+        $this->assertSame([
+            "name" => "領域驅動設計：軟體核心複雜度的解決方法 (Domain-Driven Design: Tackling Complexity in the Heart of Software)",
+            "lang" => "繁體中文",
+            "author" => "Eric Evans",
+            "publisher" => "博碩文化",
+            "publishDate" => "2019-05-13",
+            "price" => "530",
+            "link" => "https://www.tenlong.com.tw/products/9789864343874?list_name=srh",
+            "picture" => "https://cf-assets2.tenlong.com.tw/products/images/000/130/774/medium/9789864343874_bc.jpg?1554177163",
+        ], $book);
     }
 
     /**
-     * TODO: 完成第二個使用案例
+     * 以 書籍分類 爬取書籍內容（多本）
      *
      * 爬蟲的目標 URL: https://www.tenlong.com.tw/categories/object-oriented
      */
+    public function testGetBooksByCategory()
+    {
+        /** @given 要搜集的分類名稱：object-oriented */
+        $category = 'object-oriented';
+
+        /** @when 系統透過爬蟲向天瓏圖書執行關鍵字搜尋 */
+        $crawler = new TenlongCrawler();
+        $books = $crawler->getBooksByCategory($category);
+
+        /** @then 爬蟲找到書籍並擷取出書籍內容 */
+        $this->assertContains(
+            [
+                "name" => "UML3 & XML3 物件導向 程式設計寶典 (舊名: UML3 & XML3 物件導向系統分析與設計)",
+                "link" => "https://www.tenlong.com.tw/products/9789576154348?list_name=c-object-oriented",
+                "picture" => "https://cf-assets2.tenlong.com.tw/products/images/000/151/629/medium/47166-RGB.jpg?1597825422",
+            ],
+            $books
+        );
+    }
 }
